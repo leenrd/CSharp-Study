8a114ee297f00badb90713efaef66163053848b5
Leenard <lolleenard0@gmail.com>
2023-12-04 15:30:55 +0800

    chore: setup

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace app {
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
