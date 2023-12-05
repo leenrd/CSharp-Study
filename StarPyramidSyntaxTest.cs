cb971f8462671839882e5d84810e8c3e2ddccfe6
Leenard <lolleenard0@gmail.com>
2023-12-05 13:24:45 +0800

    add: StarPyramidTest

---
+++ StarPyramidSyntaxTest.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace app {
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("How tall is the pyramid?: ");
+            string? input = Console.ReadLine();
+            int? num = input?.Length > 0 ? int.Parse(input) : null;
+
+            for (int row = 0; row <= num; row++)
+            {
+                for (int col = 0; col < row; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
